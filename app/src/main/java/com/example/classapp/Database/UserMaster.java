4e1edc3d5af507ce3788d3ea650e256c2dd1a20d
PrapthaNimesh <it18217032@my.sliit.lk>
2020-05-19 20:14:59 +0530

    ClassApp(tutorial 04)

---
+++ app/src/main/java/com/example/classapp/Database/UserMaster.java
@@ -0,0 +1,18 @@
+package com.example.classapp.Database;
+
+import android.provider.BaseColumns;
+
+public final class UserMaster {
+
+    private UserMaster() {
+    }
+
+    public static class Users implements BaseColumns {
+
+        public static final String TABLE_NAME = "users";
+        public static final String COLUMN_NAME_USERNAME = "username";
+        public static final String COLUMN_NAME_PASSWORD = "password";
+    }
+
+
+}
